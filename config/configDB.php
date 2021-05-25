266424c13ce4b027dd8b0985bb6d3fe5cb575c7e
bea93 <bmerinomacia@gmail.com>
2021-05-25 20:41:06 +0200

    25/05/2021 modificación vista mto Departamentos

--- config/configDB.php
+++ config/configDB.php
@@ -1,15 +1,15 @@
 <?php
-//CONFIGURACIÓN ENTORNO DE DESARROLLO
+/*CONFIGURACIÓN ENTORNO DE DESARROLLO
 define('DNS','mysql:host=192.168.20.19:3306;dbname=DAW213AplicacionFinal');
 define('USER','usuarioDAW213AplicacionFinal');
 define('PASSWORD','paso'); 
 
-/*CONFIGURACIÓN CASA
+//CONFIGURACIÓN CASA
 define('DNS', 'mysql:host=192.168.0.200;dbname=DAW213AplicacionFinal');
 define('USER', 'usuarioDAW213AplicacionFinal');
-define('PASSWORD', 'P@ssw0rd');
+define('PASSWORD', 'P@ssw0rd');*/
 
 //CONFIGURACIÓN 1&1
 define('DNS','mysql:host=db5000278681.hosting-data.io;dbname=dbs272025');
 define('USER','dbu63905');
-define('PASSWORD','Covid1234$');*/
+define('PASSWORD','Covid1234$');
